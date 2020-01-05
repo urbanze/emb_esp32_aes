ac4b2a34b7276aa69f86a65e63aea32766a049c2
urbanze <jvitor-2005@hotmail.com>
2020-01-05 10:41:25 -0300

    ECB/CBC >16B support

--- cbc.ino
+++ cbc.ino
@@ -1,52 +1,54 @@
 #include <mbedtls\aes.h>
 
+
 mbedtls_aes_context aes;
 
 unsigned char key[] = "1234567890123456";
 unsigned char  iv[] = "1234567890123456";
 
-
-String bla = "Embarcados";
+char bla[] = "0123456789012345678901234567890123456789012345678901234567890123456789";
 unsigned char out[16];
-unsigned char inp[16]; 
+unsigned char inp[16];
+
+
 
 void setup()
 {
 	Serial.begin(115200);
 
-	int size = sizeof(bla);
-
-	for (int i = 0; i < size; i++)
-	{
-		inp[i] = bla[i];
-	}
-
-	for (int i = size; i < 16; i++)
-	{
-		inp[i] = 0;
-	}
-	
 	mbedtls_aes_init(&aes);
 	mbedtls_aes_setkey_enc(&aes, key, 128);
 }
 
 void loop()
 {
-	Serial.print("\n\nIV: ");
-	for (int i = 0; i < 16; i++)
-	{
-		Serial.print(iv[i], HEX);
-		Serial.print(" ");
-	}
+    Serial.printf("\r\n\r\nPlaintext: ");
+    Serial.printf("%s\n", bla);
 
-	mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 16, iv, inp, out);
+    Serial.printf("Chipertext: ");
+    for (int i = 0; i < strlen(bla); i += 16)
+    {
+        if (bla[i] == 0)
+            {break;}
+        
+        memset(inp, 0, sizeof(inp));
+        memset(out, 0, sizeof(out));
 
-	Serial.printf("\nChipertext: ");
-	for (int i = 0; i < 16; i++)
-	{
-		Serial.print(out[i], HEX);
-		Serial.print(" ");
-	}
+        for (int j = (0 + i); j < (16 + i); j++)
+        {
+            if (bla[j] != 0)
+                {inp[j-i] = bla[j];}
+            else
+                {break;}
+        }
 
-	delay(5000);
+        mbedtls_aes_crypt_cbc(&aes, MBEDTLS_AES_ENCRYPT, 16, iv, inp, out);
+        
+        for (int j = 0; j < 16; j++)
+        {
+           Serial.printf("%02x ", out[j]);
+        }
+    }
+
+    delay(5000);
 }
